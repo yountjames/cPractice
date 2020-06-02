deb5b8915bd9473521f43e4df4b44d5059719aa0
yountjames <sparky.1984@hotmail.com>
2020-06-01 22:24:30 -0700

    Initial commit

---
+++ cPractice/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main()
+{
+	char name[] = "James";
+	int8_t age = 35;
+	float floatnum;
+	floatnum = 4.3f;
+	printf("Your name is %s \n", name);
+	printf("You are %d years old \n", age);
+	printf("Here is a floating number: %f", floatnum);
+
+
+	//char str2[30];
+	//scanf_s("%s", str2);
+
+	return 0;
+
+}
