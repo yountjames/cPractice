a4256a26d14d99a8bcd515ff2d02eb9953860fe4
yountjames <sparky.1984@hotmail.com>
2020-06-01 23:27:39 -0700

    functions added

--- cPractice/main.c
+++ cPractice/main.c
@@ -2,20 +2,37 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+void basics(char name[]);
+
 int main()
 {
-	char name[] = "James";
-	int8_t age = 35;
+	basics("James");
+
+
+
+	return 0;
+
+}
+
+void basics(char name[])
+{
+	const int8_t age = 35;  //usage of constant
 	float floatnum;
-	floatnum = 4.3f;
+	floatnum = 4.3f; //have to put f after number otherwise it thinks it's a double
+	int myage;
 	printf("Your name is %s \n", name);
 	printf("You are %d years old \n", age);
-	printf("Here is a floating number: %f", floatnum);
+	printf("Here is a floating number: %f\n", floatnum);
+
+	printf("Enter your age: ");
+	scanf_s("%d", &myage);
+	printf("You are %d", myage);
+
+	int numbers[] = { 2, 4, 6, 8 };
+	printf("\nArray index 2 is %d", numbers[2]);
 
 
 	//char str2[30];
 	//scanf_s("%s", str2);
-
-	return 0;
 
 }
